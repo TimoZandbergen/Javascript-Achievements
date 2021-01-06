4ac7ed2ebfbf57f79c9b52a8e5fd673f66a0ff5b
Timo Zandbergen <zandbergentimo@gmail.com>
2021-01-06 11:47:01 +0100

    push

---
+++ les5/F1M2BO-Quiz-Javascript/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Random 1",
+    "quizMetaData":
+    {
+        "title": "Random Questions",
+        "imageURI": "https://www.dewestkrant.nl/wp-content/uploads/2017/05/IMG_0674-e1496049647937.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Where is the main building of Mediacollege.  ",
+				"answers": [{
+						"answer": "Haarlem",
+						"feedback": false
+					},
+					{
+						"answer": "Amsterdam",
+						"feedback": true
+					},
+					{
+						"answer": "Hong Kong",
+						"feedback": false
+					},
+					{
+						"answer": "Rotterdam",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "What is my last name ",
+				"answers": [{
+						"answer": "Joost",
+						"feedback": false
+					},
+					{
+						"answer": "Zandheuvel",
+						"feedback": false
+					},
+					{
+						"answer": "Zandbergen",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "What is the name of the best Call of Duty?",
+				"answers": [{
+						"answer": "Black ops 2",
+						"feedback": true
+					},
+					{
+						"answer": "Modern Warfare 2",
+						"feedback": true
+					},
+					{
+						"answer": "Ghost",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
